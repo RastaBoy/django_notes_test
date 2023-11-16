f65d0b264b27333ee621986f9bcf02aa658244bf
RastaBoy <trustyone3224@gmail.com>
2023-11-16 16:44:36 +0600

    models, migrates

--- notessite/notes/models.py
+++ notessite/notes/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=128)
+    email = models.EmailField()
+    password = models.CharField(max_length=255) # hash из email + password
+
+
 class Note(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     dt_create = models.DateTimeField(auto_now_add=True)
     dt_update = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 
